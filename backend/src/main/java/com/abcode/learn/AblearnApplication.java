b19d6dd2b1c8a22bd1c034064f27935ad8911ee0
Arthur Bruno Moreira Barbosa <arthur.barbosa@ITBL000531D.itbam.org.br>
2021-02-18 08:39:06 -0400

    Project created

---
+++ backend/src/main/java/com/abcode/learn/AblearnApplication.java
@@ -0,0 +1,13 @@
+package com.abcode.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AblearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AblearnApplication.class, args);
+	}
+
+}
